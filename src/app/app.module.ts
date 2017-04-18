0df2e1844ee0053f2a73eb7df26c63d6920ec762
Natalie9 <natalietolentino9@gmail.com>
2017-04-18 15:46:41 -0300

    inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { AngularFireModule } from "angularfire2";
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDD-qNetwLnNbk9o67zZGxGMPfrj9x6EAQ",
+    authDomain: "chatfire-d5346.firebaseapp.com",
+    databaseURL: "https://chatfire-d5346.firebaseio.com",
+    projectId: "chatfire-d5346",
+    storageBucket: "chatfire-d5346.appspot.com",
+    messagingSenderId: "864986088445"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  lista: FirebaseListObservable<any>;
+  mensagem: string;
+
+  constructor(public af: AngularFire) {
+    this.lista = af.database.list('https://chatfire-d5346.firebaseio.com/chat');
+  }
+
+  enviar(){
+    let m = {
+      texto: this.mensagem,
+      data: new Date()
+    };
+    this.lista.push(m).then(() => {
+      this.mensagem = "";
+    });
+}
+}
